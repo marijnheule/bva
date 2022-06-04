f38fe25dfcc9c3a58fa62a08301fede2fd5c8e5c
Marienus Heule <mheule@SCS003B8.SP.CS.CMU.EDU>
2022-06-04 09:38:36 +0200

    run

---
+++ run.sh
@@ -0,0 +1,16 @@
+INPUT=$1
+
+./formula -method=BVA2 -file= $INPUT -print=1 -limit=-1 | grep -v "UNKNOWN" > tmp-$$.txt
+
+cat tmp-$$.txt | grep "r " | sed 's|r ||' > proof-$$.drat
+cat tmp-$$.txt | grep -v "r " > formula-$$.cnf
+rm tmp-$$.txt
+
+# cadical formula-$$.cnf formula-$$.drat --no-binary
+
+# cat formula-$$.drat >> proof-$$.drat
+
+# ~/drat-trim/drat-trim $INPUT proof-$$.drat
+
+
+
